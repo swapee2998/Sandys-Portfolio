3ff08eafe989ba09ee437d7520ff2a3fbcd22f0f
swapnil <schaure3@gmail.com>
2022-07-08 18:08:38 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// let hero = document.querySelectorAll('hero');
+// let navlinks = document.querySelectorAll('nav ul li' );
+// let skills =  document.querySelectorAll('skills-bar')
+ 
+
+// window.onscroll = () =>{
+
+//     skills.forEach(sec =>{
+
+//        let top = window.scrollY;
+//        let offset = sec.offsetTop;
+//        let height = sec.offsetHeight;
+
+//        if(top >= offset && top < offset + height){
+//         navlinks.forEach(links =>{
+
+//             links.classList.remove('active');
+//             // document.querySelector('nav ul li[href*=' + id + ']').classList.add('active');
+//             document.querySelector('skills-bar').classList.add('active');
+
+
+
+//         });
+//        };
+
+
+
+
+//     });
+
+
+
+// }
